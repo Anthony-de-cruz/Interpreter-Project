abe008c00af626000aa874b2fe45c286a7b91e08
Anthony-de-cruz <anthony.decruz@proton.me>
2025-10-06 15:19:27 +0100

    Wrote a series of basic unit tests.

--- src/Interpreter-Tests/Tests.fs
+++ src/Interpreter-Tests/Tests.fs
@@ -1,8 +1,35 @@
 ﻿module Tests
 
-open System
 open Xunit
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Basic Integer Addition Valid`` () =
+    let oList = Interpreter.lexer "5 + 3"
+    Interpreter.parser oList |> ignore
+    let Out = Interpreter.parseNeval oList
+    Assert.True(8 = snd Out)
+    
+[<Fact>]
+let ``Basic Integer Addition Invalid`` () =
+    let oList1 = Interpreter.lexer "5 +"
+    Assert.Throws<System.Exception>(fun () -> Interpreter.parser oList1 |> ignore) |> ignore
+    
+    let oList0 = Interpreter.lexer "5 + 3 +"
+    Assert.Throws<System.Exception>(fun () -> Interpreter.parser oList0 |> ignore) |> ignore
+    
+    let oList1 = Interpreter.lexer "+"
+    Assert.Throws<System.Exception>(fun () -> Interpreter.parser oList1 |> ignore) |> ignore
+    
+[<Fact>]
+let ``Basic Integer Multiplication Valid`` () =
+    let oList = Interpreter.lexer "3 * 3"
+    Interpreter.parser oList |> ignore
+    let Out = Interpreter.parseNeval oList
+    Assert.True(9 = snd Out)
+    
+[<Fact>]
+let ``Basic Integer Division Valid`` () =
+    let oList = Interpreter.lexer "6 / 3"
+    Interpreter.parser oList |> ignore
+    let Out = Interpreter.parseNeval oList
+    Assert.True(2 = snd Out)
