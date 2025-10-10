6ffb0f23ba67c02b4a44734eb33accd7338266b8
Anthony de Cruz <anthonydecruz@proton.me>
2025-10-10 17:23:30 +0100

    Added basic interpreter functionality to GUI.

--- src/GUI/MainWindow.xaml.cs
+++ src/GUI/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
 ﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.FSharp.Collections;
 
 namespace GUI;
 
@@ -16,8 +10,57 @@ namespace GUI;
 /// </summary>
 public partial class MainWindow : Window
 {
+    /// <summary>
+    /// Construct <see cref="MainWindow"/>.
+    /// </summary>
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// Handle expression text change.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void exprTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+    {
+        calculateButton.IsEnabled = exprTextBox.Text.Length > 0;
+    }
+
+    /// <summary>
+    /// Handle calculate button click.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void exprButton_Click(object sender, RoutedEventArgs e)
+    {
+        FSharpList<Interpreter.terminal> lexed;
+        int? result;
+        try
+        {
+            lexed = Interpreter.lexer(exprTextBox.Text);
+            Interpreter.parser(lexed);
+            (_, result) = Interpreter.parseNeval(lexed);
+            
+        }
+        // Todo - Add new exception types to Interpreter to give better feedback.
+        catch (Exception ex)
+        {
+            outputTextBlock.Foreground = Brushes.Red;
+            outputTextBlock.Text = ex.ToString();
+            return;
+        }
+
+        // Todo - Investigate why these values return nullable.
+        if (result is null)
+            throw new Exception("WHAT");
+        
+        StringBuilder terminalListBuilder = new StringBuilder();
+        foreach (Interpreter.terminal i in lexed)
+            terminalListBuilder.Append($"{i} ");
+        
+        outputTextBlock.Foreground = Brushes.Black;
+        outputTextBlock.Text = $"{terminalListBuilder}= {result}";
     }
 }
