9de1ca0a0d8854b1e6266ca8cfcaaf60c9021c23
Anthony de Cruz <anthonydecruz@proton.me>
2025-10-10 15:31:51 +0100

    Updated unit tests to be more idiomatic

--- src/Interpreter-Tests/ParserTests.fs
+++ src/Interpreter-Tests/ParserTests.fs
@@ -4,44 +4,72 @@ open Xunit
 
 [<Fact>]
 let ``Basic Integer Addition Valid`` () =
-    let oList = Interpreter.lexer "5 + 3"
-    Interpreter.parser oList |> ignore
-    let Out = Interpreter.parseNeval oList
-    Assert.True(8 = snd Out)
+    Interpreter.lexer "5 + 3"
+    |> fun o ->
+        Interpreter.parser o |> ignore
+        Interpreter.parseNeval o
+    |> snd = 8
+    |> Assert.True
     
+    Interpreter.lexer "200 + 13 + 45"
+    |> fun o ->
+        Interpreter.parser o |> ignore
+        Interpreter.parseNeval o
+    |> snd = 258
+    |> Assert.True
+
 [<Fact>]
 let ``Basic Integer Addition Invalid`` () =
-    Assert.Throws<System.Exception>(fun () ->
-        Interpreter.lexer "5 +"
-        |> Interpreter.parser
-        |> ignore
-    ) |> ignore 
-    Assert.Throws<System.Exception>(fun () ->
-        Interpreter.lexer "5 + 3 +"
-        |> Interpreter.parser
-        |> ignore
-    ) |> ignore 
-    Assert.Throws<System.Exception>(fun () ->
+    fun () ->
         Interpreter.lexer "+"
         |> Interpreter.parser
         |> ignore
-    ) |> ignore 
-    
+    |> Assert.Throws<System.Exception>
+    |> ignore
+
+    fun () ->
+        Interpreter.lexer "5 +"
+        |> Interpreter.parser
+        |> ignore
+    |> Assert.Throws<System.Exception>
+    |> ignore
+
+    fun () ->
+        Interpreter.lexer "5 + 3 +"
+        |> Interpreter.parser
+        |> ignore
+    |> Assert.Throws<System.Exception>
+    |> ignore
+
 [<Fact>]
 let ``Basic Integer Multiplication Valid`` () =
     Interpreter.lexer "3 * 3"
-    |> fun o -> Interpreter.parser o |> ignore; Interpreter.parseNeval o
+    |> fun o ->
+        Interpreter.parser o |> ignore
+        Interpreter.parseNeval o
     |> snd = 9
     |> Assert.True
     
-  // let oList = Interpreter.lexer "3 * 3"
-  // Interpreter.parser oList |> ignore
-  // let Out = Interpreter.parseNeval oList
-  // Assert.True(9 = snd Out)
-    
+    Interpreter.lexer "8 * 4 * 3"
+    |> fun o ->
+        Interpreter.parser o |> ignore
+        Interpreter.parseNeval o
+    |> snd = 96
+    |> Assert.True
+
 [<Fact>]
 let ``Basic Integer Division Valid`` () =
-    let oList = Interpreter.lexer "6 / 3"
-    Interpreter.parser oList |> ignore
-    let Out = Interpreter.parseNeval oList
-    Assert.True(2 = snd Out)
+    Interpreter.lexer "6 / 3"
+    |> fun o ->
+        Interpreter.parser o |> ignore
+        Interpreter.parseNeval o
+    |> snd = 2
+    |> Assert.True
+    
+    Interpreter.lexer "12 / 3 / 2"
+    |> fun o ->
+        Interpreter.parser o |> ignore
+        Interpreter.parseNeval o
+    |> snd = 2
+    |> Assert.True
+
